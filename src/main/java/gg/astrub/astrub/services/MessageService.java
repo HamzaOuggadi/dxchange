238d60f185bed8773aef1c4d84ac334606df8bdc
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2023-01-16 17:03:42 +0100

    Added Listing for sendMessage method in MessageService

--- src/main/java/gg/astrub/astrub/services/MessageService.java
+++ src/main/java/gg/astrub/astrub/services/MessageService.java
@@ -1,9 +1,10 @@
 package gg.astrub.astrub.services;
 
+import gg.astrub.astrub.entities.Listing;
 import gg.astrub.astrub.entities.Message;
 import gg.astrub.astrub.entities.User;
 
 public interface MessageService {
-    Message sendMessage(String messageContent, User userSender, User userRecipient);
+    Message sendMessage(String messageContent, User userSender, User userRecipient, Listing listing);
     void deleteMessage(Long messageId);
 }
