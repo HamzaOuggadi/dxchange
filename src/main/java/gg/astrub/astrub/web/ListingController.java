d1158f5f388d9a9530586283042c8b69ac60d11a
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2023-01-21 09:17:16 +0100

    Comments for the next improvements

--- src/main/java/gg/astrub/astrub/web/ListingController.java
+++ src/main/java/gg/astrub/astrub/web/ListingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/* Need to test every endpoint before Spring Security and Authentication */
 @RestController
 @AllArgsConstructor
 public class ListingController {
