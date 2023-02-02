7c7784424dd126bcdd64025c55a419eb96580a71
hamza <hamza.ouggadi@gmail.com>
2023-02-02 10:26:35 +0100

    Adding getUserProfilePhoto

--- src/main/java/gg/astrub/astrub/web/UserController.java
+++ src/main/java/gg/astrub/astrub/web/UserController.java
@@ -7,6 +7,7 @@ import gg.astrub.astrub.services.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,8 +36,12 @@ public class UserController {
         userService.editUser(user);
     }
     @PostMapping("/users/addPhoto")
-    public void addUserProfilePhoto(@RequestParam(name = "userId") Long userId,@RequestParam(name = "profilePhoto") MultipartFile file) throws FileException, UserException, IOException {
+    public String addUserProfilePhoto(@RequestParam(name = "userId") Long userId,
+                                    @RequestParam(name = "profilePhoto") MultipartFile file,
+                                    RedirectAttributes redirectAttributes) throws FileException, UserException, IOException {
         userService.setUserProfilePhoto(userId, file);
+        redirectAttributes.addFlashAttribute("message", "File Upload Successful.");
+        return "redirect:/";
     }
     @DeleteMapping("/users/removeUser/{userId}")
     public void removeUserById(@PathVariable Long userId) throws UserException {
